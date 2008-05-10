d42571561f87978fdca39c5e963bc6af9d1bb8d8
mdawaffe <mdawaffe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-05-10 02:05:07 +0000

    version 1 - compatible with WP 2.3+
    git-svn-id: https://plugins.svn.wordpress.org/just-one-category/trunk@45059 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- just_one_cat.php
+++ just_one_cat.php
@@ -1,32 +1,35 @@
 <?php
 /*
 Plugin Name: Just One Category
-Version: 0.3
+Version: 1.0
 Plugin URI: http://www.blogwaffe.com:8000/2005/05/31/294/
-Description: On a category's archive page do not show posts from that category's children categories unless those posts are also a member of the original category in question.  Based on Front Page Categories by Ryan Boren
-Author: Michael D. Adams
+Description: On a category's archive page do not show posts from that category's children categories unless those posts are also a member of the original category in question.  Based on Front Page Categories by Ryan Boren.
+Author: Michael D Adams
 Author URI: http://www.blogwaffe.com:8000/
-*/ 
 
-/*
 Released under the GPL license
 http://www.gnu.org/licenses/gpl.txt
 */
 
 function joc_where($where) {
+	global $wpdb;
 	if ( !is_category() )
 		return $where;
 
-	if ( $cat_array = rtrim(get_query_var('category_name'), ' /') ) : // trim a trailing slash if necessary.
-		$cat_array = explode('/', $cat_array);
-		$cat = array_pop($cat_array);
-		$where .= " AND category_nicename = '$cat'";
-	elseif ( $cat = rtrim(get_query_var('cat'), ' /') ) :
-		$where .= " AND category_id = $cat";
-	endif;
+	$cat = rtrim(get_query_var('cat'), ' /');
+
+	if ( !is_numeric($cat) || $cat < 1 )
+		return $where;
+
+	$field = preg_quote( "$wpdb->term_taxonomy.term_id", '#' );
+
+	$just_one .= $wpdb->prepare( " AND $wpdb->term_taxonomy.term_id = %d ", $cat );
+	if ( preg_match( "#AND\s+$field\s+IN\s*\(\s*(?:['\"]?\d+['\"]?\s*,\s*)*['\"]?\d+['\"]?\s*\)#", $where, $matches ) )
+		$where = str_replace( $matches[0], $just_one, $where );
+	else
+		$where .= $just_one;
+
 	return $where;
 }
 
 add_filter('posts_where', 'joc_where');
-
-?>
